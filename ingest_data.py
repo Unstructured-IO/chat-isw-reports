a08ad30c6f5e95ac9af564e690fa79f0407e2d50
Matt Robinson <mrobinson@unstructuredai.io>
2023-02-14 15:30:04 -0500

    update class name

--- ingest_data.py
+++ ingest_data.py
@@ -12,7 +12,7 @@ urls = [
     "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-february-8-2023",
     "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-february-9-2023",
 ]
-loader = UnstructuredURLLoader(urls=urls)
+loader = UnstructuredISWURLLoader(urls=urls)
 raw_documents = loader.load()
 
 # Split text
